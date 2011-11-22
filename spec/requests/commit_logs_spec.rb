39083a11b031f2deb2da41624254368dcf3db5ae
MisaKondo <Misa_Kondo@voyagegroup.com>
2011-11-22 18:20:03 +0900

    comment request spec

--- spec/requests/commit_logs_spec.rb
+++ spec/requests/commit_logs_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe "CommitLogs" do
+=begin
   describe "GET /commit_logs" do
     it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
@@ -8,4 +9,5 @@ describe "CommitLogs" do
       response.status.should be(200)
     end
   end
+=end
 end
--- spec/requests/types_spec.rb
+++ spec/requests/types_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe "Types" do
+=begin
   describe "GET /types" do
     it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
@@ -8,4 +9,5 @@ describe "Types" do
       response.status.should be(200)
     end
   end
+=end
 end
